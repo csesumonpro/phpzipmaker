83aeb43e727f60e097ab872ee98fb6b2680b10d6
csesumonpro <csesumonpro@gmail.com>
2022-09-04 22:53:33 +0600

    Added vendor directory

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'csesumonpro/phpzipmaker',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b192d28fca7f8c911f7aae4005555d8732395943',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'csesumonpro/phpzipmaker' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b192d28fca7f8c911f7aae4005555d8732395943',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
